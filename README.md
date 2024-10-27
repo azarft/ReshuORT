284b8f0a3d6bb31486a35ccccb3af46fd65576d4
azarft <argoazanov@gmail.com>
2024-10-27 11:46:48 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ReshuORT
---
+++ src/main/java/com/alatoo/reshu_ort/ReshuOrtApplication.java
@@ -0,0 +1,13 @@
+package com.alatoo.reshu_ort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReshuOrtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReshuOrtApplication.class, args);
+	}
+
+}
